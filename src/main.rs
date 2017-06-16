ee263b09d380097290b12c8d9b573b1b202cc3b5
mpentler <tehhustler@hotmail.com>
2017-06-16 23:49:19 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,7 @@ fn to_hex(bytes: &[u8]) -> String {
 
 fn main() {
   println!("PortLurker v{}", VERSION);
-  println!("Bart Noordervlet (http://www.mmvl.nl/)");
+  println!("Developer: Bart Noordervlet (http://www.github.com/bartnv)");
   
   let mut app = App { print_ascii: false, print_binary: false };
   let binary_matches = [
