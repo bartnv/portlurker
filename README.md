1def56ed45fbd79fb611eaf81bb3b70d78985fbb
Ubuntu <ubuntu@ip-172-31-14-201.eu-west-2.compute.internal>
2017-06-20 20:39:19 +0000

    Added SQLite DB logging feature - no error handling yet!

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Port listener / honeypot in Rust with protocol guessing and safe string display
 ## Installation
 To start, first install the latest stable version of rustc & Cargo by following the instructions at: https://www.rustup.rs/
 
-Running `cargo build` will build the project and `cargo run` will run it - you may need to do this as root to enable access to lower ports.
+For SQLite logging support you will also need to run 'sudo apt-get install libsqlite3-dev'
+
+Running `cargo build` will build the project and `cargo run` will run it - you may need to run both commands as root for reasons of file permissions and to enable access to lower ports.
 
 ## Configuration
 Configuration is done in the config.yml file, which as the extension hints at is a YAML-formatted file. It should have two top-level keys called "general" and "ports".
@@ -12,6 +14,7 @@ Configuration is done in the config.yml file, which as the extension hints at is
 The general section has subkeys for the different options you can set in the file. Current these are:
  - print_ascii (boolean): show printable ascii found in the received data
  - print_binary (boolean): show all received data as a series of integer byte values
+ - sql_logging (boolean): enable logging to an sqlite3 database file (portlurker.sqlite)
 
 The ports section contains a list of listening-port specifications. Each item in the list is itself a key-value collection. At a minimum it should have either a "tcp" key (integer) or a "udp" key (integer). Additional keys can be:
  - banner (string): send this string to each new connection on the port (often you'll need to send carriage-return and linefeed after this string; you can do that in YAML by enclosing the string in double quotes and adding \r\n at the end)
@@ -21,6 +24,7 @@ An very basic config might look like this:
 general:
  print_ascii: true
  print_binary: false
+ sql_logging: true
 ports:
  - tcp: 22
    banner: "SSH-2.0-OpenSSH_6.7p1 Debian-5+deb8u3\r\n"
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate yaml_rust;
 extern crate regex;
+extern crate rusqlite;
 
 use std::fs::File;
 use std::io;
@@ -11,6 +12,7 @@ use std::thread;
 use std::process::exit;
 use yaml_rust::YamlLoader;
 use regex::bytes::RegexSetBuilder;
+use rusqlite::Connection;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
@@ -18,7 +20,8 @@ const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
 #[derive(Copy, Clone)]
 struct App {
   print_ascii: bool,
-  print_binary: bool
+  print_binary: bool,
+  sql_logging: bool
 }
 
 fn to_hex(bytes: &[u8]) -> String {
@@ -57,7 +60,7 @@ fn main() {
   println!("{}", AUTHORS);
   println!("-----------------------------------------");
   
-  let mut app = App { print_ascii: false, print_binary: false };
+  let mut app = App { print_ascii: false, print_binary: false, sql_logging: false };
   let binary_matches = [
     ("SSL3.0 Record Protocol", r"^\x16\x03\x00..\x01"),
     ("TLS1.0 Record Protocol", r"^\x16\x03\x01..\x01"),
@@ -110,6 +113,19 @@ fn main() {
       println!("Printing binary in hexadecimal");
     }
   }
+  if !config["general"]["sql_logging"].is_badvalue() {
+    if config["general"]["sql_logging"].as_bool().unwrap() {
+      app.sql_logging = true;
+      println!("Logging events to portlurker.sqlite");
+      let conn = Connection::open("./portlurker.sqlite").unwrap();
+      conn.execute("CREATE TABLE IF NOT EXISTS connections (
+        id         INTEGER PRIMARY KEY,
+        remoteip   TEXT NOT NULL,
+        remoteport INTEGER NOT NULL,
+        localport  INTEGER NOT NULL
+      )", &[]).unwrap();
+    }
+  }
   let app = app; // Revert to immutable
 
   let mut patterns = Vec::with_capacity(binary_matches.len());
@@ -122,7 +138,7 @@ fn main() {
     .build().unwrap();
 
   println!("\nStarting listeners on the following ports:");
-  
+
   for port in config["ports"].as_vec().unwrap() {
     if !port["tcp"].is_badvalue() {
       let portno = port["tcp"].as_i64().unwrap();
@@ -144,6 +160,25 @@ fn main() {
           stream.set_write_timeout(Some(io_timeout)).expect("Failed to set write timeout on TcpStream");
           let addr = stream.peer_addr().unwrap();
           println!("CONNECT TCP {} from {}", portno, addr);
+
+          if app.sql_logging {
+            #[derive(Debug)]
+            struct LoggedConnection {
+              remoteip: String,
+              remoteport: u16,
+              localport: i64
+            }
+
+            let newdbentry = LoggedConnection {
+              remoteip: addr.ip().to_string(),
+              remoteport: addr.port(),
+              localport: portno
+            };
+
+            let conn = Connection::open("./portlurker.sqlite").unwrap();
+            conn.execute("INSERT INTO connections (remoteip, remoteport, localport) VALUES (?1, ?2, ?3)", &[&newdbentry.remoteip, &newdbentry.remoteport, &newdbentry.localport]).unwrap();
+          }
+
           let regexset = regexset.clone();
           let banner = banner.clone();
           thread::spawn(move || {
@@ -183,7 +218,7 @@ fn main() {
                     }
                     else {
                       if found {
-                        if (i-start == 1) {
+                        if i-start == 1 {
                           if (start > 0) && (buf[start-1] == 0) {
                             mbstring.push(buf[i-1] as char);
                             if !mbfound { mbfound = true; }
